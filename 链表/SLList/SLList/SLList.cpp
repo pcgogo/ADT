da042e81e02e0b7a7ade715ab850e5b8bdbb88b0
pcgogo <396622209@qq.com>
2020-09-20 19:28:22 +0800

    rewrite SLList

--- 链表/SLList/SLList/SLList.cpp
+++ 链表/SLList/SLList/SLList.cpp
@@ -10,18 +10,12 @@ int main()
 	using std::string;
 
 	{
-		string str;
-		SLList<string> sl;
-		sl.addToHead("1");
-		sl.addToTail("2");
-		sl.addToTail("3");
-		sl.deleteFromHead(str);
-		cout << str << endl;
-		sl.deleteFromTail(str);
-		cout << str << endl;
-		sl.deleteNode("2");
-		cout << sl.isInList("2") << endl;
-		
+		SLList<int> list;
+		list.addToTail(1);
+		list.addToTail(2);
+		list.deleteFromHead();
+		list.deleteFromHead();
+		cout << list.isEmpty() << endl;
 	}
 
 	cin.get();
--- 链表/SLList/SLList/SLList.h
+++ 链表/SLList/SLList/SLList.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 
-template <class T>
+template <typename T>
 class SLList
 {
 private:
@@ -12,135 +12,150 @@ private:
 	{
 	public:
 		T info;
-		Node * next;
-		Node(){ next = 0; }
-		Node(T t, Node * pn = 0){ info = t; next = pn; }
+		Node* next;
+		Node() { next = nullptr; }
+		Node(T el, Node* nptr = nullptr) { info = el; next = nptr; }
 	};
-	Node * head;
-	Node * tail;
+	Node* head;
+	Node* tail;
 public:
-	SLList(){ head = tail = 0; }
+	SLList();
+	//SLList(const SLList & list);
 	~SLList();
-	bool isEmpty()const{ return head == 0; }
-	bool addToHead(const T & item);
-	bool addToTail(const T & item);
-	bool deleteFromHead(T & item);
-	bool deleteFromTail(T & item);
-	bool deleteNode(const T & item);
-	bool isInList(const T & item)const;
+	//SLList & operator= (const SLList & list);	//返回对象将调用复制构造函数，返回引用不会
+	bool isEmpty();
+	void addToHead(T el);
+	void addToTail(T el);
+	T deleteFromHead();
+	T deleteFromTail();
+	void deleteNode(T el);
+	bool isInList(const T el) const; //非静态成员函数后面加const，表示成员函数隐含传入的this指针为const指针，决定了在该成员函数中，任意修改它所在的类的成员的操作都是不允许的
+
 };
 
-template<class T>
+
+template <typename T>
+SLList<T>::SLList()
+{
+	head = tail = nullptr;
+}
+
+
+template <typename T>
 SLList<T>::~SLList()
 {
-	Node * temp;
-	while (head != 0)
+	for (Node* temp; !isEmpty();) //先执行for循环第一个参数，然后执行第二个表达式，为True执行循环体，循环体执行完后执行第三个参数
 	{
-		temp = head;
-		head = head->next;
-		delete temp;
+		temp = head->next;
+		delete head;
+		head = temp;
 	}
 }
 
-template<class T>
-bool SLList<T>::addToHead(const T & item)
+template <typename T>
+bool SLList<T>::isEmpty()
 {
-	head = new Node(item);
-	if (tail == 0)
-		tail = head;
-	return true;
+	return head == nullptr;
 }
 
-template<class T>
-bool SLList<T>::addToTail(const T & item)
+template <typename T>
+void SLList<T>::addToHead(T el)
 {
-	if (!isEmpty())
+	head = New(el, head);
+	if (tail == nullptr)
 	{
-		tail->next = new Node(item);
+		tail = head;
+	}
+}
+
+template <typename T>
+void SLList<T>::addToTail(T el)
+{
+	if (tail != nullptr) //链表不为空
+	{
+		tail->next = new Node(el);
 		tail = tail->next;
 	}
 	else
-		head = tail = new Node(item);
-	return true;
+	{
+		head = tail = new Node(el);
+	}
 }
 
-template<class T>
-bool SLList<T>::deleteFromHead(T & item)
+template <typename T>
+T SLList<T>::deleteFromHead()
 {
-	if (isEmpty())
-		return false;
-	Node * temp = head;
-	item = head->info;
-	head = head->next;
-	delete temp;
-	if (head == 0)
-		tail = 0;
-	return true;
-}
-
-template<class T>
-bool SLList<T>::deleteFromTail(T & item)
-{
-	if (isEmpty())
-		return false;
-	item = tail->info;
+	T el = head->info;
+	Node* temp = head;
 	if (head == tail)
 	{
-		delete tail;
-		head = tail = 0;
-		return true;
+		head = tail = nullptr;
 	}
 	else
-	{
-		Node * temp;
-		for (temp = head; temp->next != tail; temp = temp->next);
-		delete tail;
-		tail = temp;
-		tail->next = 0;
-		return true;
-	}
+		head = head->next;
+	delete temp;
+	return el;
 }
 
-template<class T>
-bool SLList<T>::deleteNode(const T & item)
+
+template <typename T>
+T SLList<T>::deleteFromTail()
 {
-	if (isEmpty())
-		return false;
-	else if (head == tail && item == head->info)
+	T el = tail->info;
+	if (head == temp)
 	{
 		delete head;
-		head = tail = 0;
-		return true;
-	}
-	else if (item == head->info)
-	{
-		Node * temp = head;
-		head = head->next;
-		delete temp;
-		return true;
+		head = tail = nullptr;
 	}
 	else
 	{
-		Node *temp, *pred;
-		for (pred = head, temp = head->next; temp != 0 && item != temp->info; pred = pred->next, temp = temp->next);
-		pred->next = temp->next;
-		if (temp == tail)
-			tail = pred;
-		delete temp;
-		return true;
+		for (Node* temp = head; temp->next != tail; temp = temp->next);
+		delete tail;
+		tail = temp;
+		tail->next = nullptr;
+	}
+	return el;
+}
+
+
+template <typename T>
+void SLList<T>::deleteNode(T el)
+{
+	if (head != nullptr)
+	{
+		if (head == tail && el == head->info)
+		{
+			delete head;
+			head = tail = nullptr;
+		}
+		else if (el == head->info)
+		{
+			Node* temp = head;
+			head = head->next;
+			delete temp;
+		}
+		else
+		{
+			Node* pred, *temp;
+			for (pred = head, temp = head->next; temp != nullptr && temp->info != el; pred = pred->next, temp = temp->next);
+			if (temp != nullptr)
+			{
+				pred->next = temp->next;
+				if (temp == tail)
+					tail = pred;
+				delete temp;
+			}
+
+		}
 	}
 }
 
-template<class T>
-bool SLList<T>::isInList(const T & item)const
+template <typename T>
+bool SLList<T>::isInList(const T el)const
 {
-	if (isEmpty())
-		return false;
-	Node *temp;
-	for ( temp = head; temp->info != item; temp = temp->next);
-	return temp != 0;//最后一个节点的next为空指针
+	for (Node* temp = head; (!(el == head->info)) && temp != nullptr; temp = temp->next);
+	return temp != nullptr;
 }
-
 
 
 
